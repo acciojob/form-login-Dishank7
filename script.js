ba2e8e706be1db7b732f2cf4619220d8eb54e3d2
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-03 23:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	 let name = document.getElementsByTagName("input")[0];
+	let lname = document.getElementsByTagName("input")[1];
+    nameValue = name.value;
+    lnameValue = lname.value;
+    //console.log(nameValue,lnameValue);
+	alert(nameValue +" "+ lnameValue);
 
 }
